9a19ab04be161b3b45348543a43a15eaaa35bc02
Vika Mika <oneilcode111@gmail.com>
2025-07-06 11:44:19 +1000

    Добавлен компонент loader

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,22 +1,28 @@
 import { useEffect, useState } from 'react';
 import { WishCard } from '../../components/WishCard';
 import cls from './HomePage.module.css';
+import { Loader } from '../../components/Loader';
 
 const WISHES_URL = "http://localhost:8801"
 
 export const HomePage = () => {
   const [wishes, setWishes] = useState([])
+  const [isLoading, setLoading] = useState(false)
 
   const getWishesCards = async () => {
     try {
+      setLoading(true)
+
+      await new Promise((res) => setTimeout(res, 2000))
       const response = await fetch(`${WISHES_URL}/wishes`)
       const wishes = await response.json()
 
       setWishes(wishes)
-      console.log(wishes);
          
     } catch (error) {
-      console.log(error);
+      console.error(error);
+    } finally {
+      setLoading(false)
     }
   }
 
@@ -25,12 +31,16 @@ export const HomePage = () => {
   }, [])
 
   return (
-    <div className={cls.wishesWrapper}>
-      {
-        wishes.map((wish, index) => {
-          return <WishCard key={index} wish={wish}/>
-        })
-      }    
-    </div>
+    <> 
+      {isLoading && <Loader />}
+      
+      <div className={cls.wishesWrapper}>
+        {
+          wishes.map((wish, index) => {
+            return <WishCard key={index} wish={wish}/>
+          })
+        }    
+      </div>
+    </>
   );
 };
